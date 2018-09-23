892735f2d7e78960f444635c1fc17aeccab599aa
Angel Torres <ator89@outlook.com>
2018-09-23 02:25:47 -0600

    if

--- seis.cpp
+++ seis.cpp
@@ -5,6 +5,21 @@ using namespace std;
 
 int main()
 {
+    bool estado=true;
+    if(estado==true){
+       cout<<"On"<<endl;
+    }else{cout<<"off"<<endl;}
+                     
+    int nota;
+    cout<< "Indique la nota del alumno de 0 a 10: ";
+    cin>>nota;
+    if(nota >=0 && nota <5)
+    {
+       cout << "El alumno ha sido suspendido"<<endl;
+    }else{
+       cout<<"Aprobado"<<endl;
+    }
+    
     
     system("pause");
     return 0;

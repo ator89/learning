b582196cc8d0bd925da74bfd91e8c19684ade8ad
Angel Torres <ator89@outlook.com>
2018-09-23 17:08:11 -0600

    once

---
+++ once.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+struct grupo{
+   int num;
+   string n;
+   vector<string> arr;
+}
+
+
+int main()
+{
+  
+    
+  system("pause");
+  return 0;
+}

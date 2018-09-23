4081b247b89b07dd29c67aef050c2963de5f6a35
Angel Torres <ator89@outlook.com>
2018-09-22 21:20:12 -0600

    Uno

---
+++ uno.cpp
@@ -0,0 +1,19 @@
+//
+/*
+uno.cpp
+*/
+#include <iostream>
+
+//nos permite acceder a los elementos de la libreria sin incluir el objeto std::
+using namespace std;
+
+//Funcion principal
+int main()
+{
+    cout << "Hola\n" << "hola"<< endl;
+    cout << 1 << 2+3 << "test"<<endl;
+    
+    cin.get();
+    //system("pause");//mantiene la consola en pausa
+    return 0;//main debe retornar 0
+}

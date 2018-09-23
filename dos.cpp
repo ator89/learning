d25f772bb945337a95505d9a024a4f81c6515125
Angel Torres <ator89@outlook.com>
2018-09-22 21:52:20 -0600

    char

--- dos.cpp
+++ dos.cpp
@@ -15,6 +15,20 @@ int main()
     
     cout << hola << " contiene el siguiente numero de caracteres... "<<strlen(hola)<<"\n";
     
+    //caracteres sin longitud definida
+    char *cadena = "cualquier longitud";
+    cout<< cadena<< "\n";
+    
+    char *variable1 = "hola ", *otra="nu";
+    
+    cout << variable1 << otra<<endl;
+    
+    char otraCosa[256];
+    cout << "Cual es tu nombre: ";
+    cin.getline(otraCosa, sizeof(otraCosa));
+    cout << "Hola " << otraCosa << endl;
+    
+    
     system("pause");
   return 0;
 }

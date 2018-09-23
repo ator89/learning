c0b7f48a39fd796f6e7a2a29976aee40d669a066
Angel Torres <ator89@outlook.com>
2018-09-23 00:21:10 -0600

    tres

--- tres.cpp
+++ tres.cpp
@@ -4,7 +4,22 @@ using namespace std;
 
 int main()
 {
-    int num = 0;
+    int num = 1;
+    
+    cout << num << endl;
+    
+    int num2(-20);
+    cout << num2 << endl;
+    
+    int n1, n2, n3, total;
+    n1 = 4;
+    n2 = 2;
+    n3 = 5;
+    total = n1+n2+n3;
+    cout << n1 << "+"<<n3<<"="<<total<<endl;
+
+    int array[] = {4,5,6,8};
+    cout << "Elemento 3 del array "<<array[2]<<endl;
     
     
     

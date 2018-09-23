38f4528dfb0768de44f958c0f90a2f4d1c87d803
Angel Torres <ator89@outlook.com>
2018-09-23 02:55:05 -0600

    for

--- ocho.cpp
+++ ocho.cpp
@@ -11,6 +11,22 @@ int main()
      cout << i <<" - "<< array[i]<<endl;
   }
   
+  
+  char arraychar[5] = "hola";
+  
+  for (int t=0;t<strlen(arraychar);t++){
+      if(t==2)continue;
+      cout << t << " - " << arraychar[t]<<endl;
+     }
+  
+  
+  int x;
+  int y;
+  for( x=0,y=10; x!=y; x++, y--){
+     cout << "x:" << x << ", y:" << y << endl;
+  }
+  
+  
   system("pause");
   return 0;
 }

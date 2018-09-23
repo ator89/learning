db259a9a566639bd06df3a0cdf30aa9e2993b7b9
Angel Torres <ator89@outlook.com>
2018-09-23 02:37:20 -0600

    7 - switch

--- siete.cpp
+++ siete.cpp
@@ -28,6 +28,35 @@ int main()
     
     }
     
+    char opcion;
+    
+    cout<< "Menu\n";
+    cout<< "a - opcion 1"<<endl;
+    cout<< "b - opcion 2"<<endl;
+    cout<< "c - opcion 3"<<endl;
+    cout<< "d - opcion 4"<<endl;
+    cin>>opcion;
+    
+    switch(opcion){
+       case 'a':
+            cout << "si "<< opcion<<endl;
+            break;
+       case 'b':
+            cout << "si"<<endl;
+            break;
+       case 'c':
+            cout << "si"<<endl;
+            break;
+       case 'd':
+            cout << "si"<<endl;
+            break;
+       default:
+               cout << "No existe"<<endl;
+            break;     
+    
+    }
+    
+    
     system("pause");
     return 0;
 }

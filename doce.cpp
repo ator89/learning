edf6247123457056b69aef4c9638383226634559
Angel Torres <ator89@outlook.com>
2018-09-23 18:46:56 -0600

    Funciones

--- doce.cpp
+++ doce.cpp
@@ -6,12 +6,21 @@ using namespace std;
 
 
 int number(){
-    
+    return 10;
+}
+
+string nombre(string nombre){
+    return nombre;       
 }
 
 int main()
 {
+  cout << "Valor de number:" << number () << endl;
+  string nom;
+  cout << "Wie heisst du?" <<endl;
+  cin >> nom;
   
+  cout << "Hola " << nombre(nom) << endl;
   
   system("pause");
   return 0;

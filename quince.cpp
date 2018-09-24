6976ef44ea9ace62213c59e645455244785fa895
Angel Torres <ator89@outlook.com>
2018-09-24 01:08:51 -0600

    neu

--- quince.cpp
+++ quince.cpp
@@ -6,6 +6,11 @@ using namespace std;
 //es un array con los argumentos pasados en la línea de comando.
 int main(int argc, char *argv[])
 {
+  cout<< "Numero de argumentos pasados: " << argc << endl;
+  cout << "listas de argumentos: " << endl;
+  for(int i=0; i<argc; i++){
+     cout<< i << "argumento: " << argv[i] << endl;
+  }
 
   system("pause");
   return 0;

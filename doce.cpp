bf2140dbca39a15cf098e0ea0604c6b789a5e57a
Angel Torres <ator89@outlook.com>
2018-09-23 23:15:07 -0600

    doce

--- doce.cpp
+++ doce.cpp
@@ -13,6 +13,24 @@ string nombre(string nombre){
     return nombre;       
 }
 
+int numeros(int n){
+    return n;
+}
+
+//funcion con argumento opcional
+int otro(int n, int opt = 10){
+   return n+opt;
+}
+
+void imprimirl(string palabras=""){
+     cout<<palabras<<endl;
+}
+
+bool estado(string s){
+   if(s=="ok") return true;
+   else return false;     
+}
+
 int main()
 {
   cout << "Valor de number:" << number () << endl;
@@ -21,6 +39,13 @@ int main()
   cin >> nom;
   
   cout << "Hola " << nombre(nom) << endl;
+  
+  cout << "Funcion nueva " << otro(3)<<endl;
+  
+  imprimirl("hola");
+  
+  if(estado("ok")==true) cout << "Activado" << endl;
+  else cout << "Desactivado" << endl;
   
   system("pause");
   return 0;

2b4e29e961ecfc057dca4d5595bf3198317fbdf4
Angel Torres <ator89@outlook.com>
2018-09-24 17:56:39 -0600

    clases

--- persona.h
+++ persona.h
@@ -5,8 +5,26 @@ using namespace std;
 class Persona{
       private:
               int _ojos;
+              int _brazos;
+              int _cabeza;
               
       public:
-             int ojos(){_ojos = 2;return _ojos;};
-      
+             int ojos(){
+                 _ojos = 2;
+                 
+                 return _ojos;
+                 };
+                 
+             int brazos();
+             int cabeza() { _cabeza=1; return _cabeza; };
+             static string color_ojos;
       };
+      
+int Persona::brazos()
+{
+   _brazos = 2;
+   return _brazos;
+}
+
+string Persona::color_ojos = "negro";
+
--- siebzehn.cpp
+++ siebzehn.cpp
@@ -7,6 +7,9 @@ int main(int argc, char *argv[]){
 
    Persona p;
    cout << "Una persona: " << p.ojos()<<endl;
+   cout << p.brazos()<<endl;
+   cout << p.cabeza() << endl;
+   cout << p.color_ojos << endl;
     
    system("pause");
    return 0;

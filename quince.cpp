6862c9a36b1265662026c57134b8383948a96910
Angel Torres <ator89@outlook.com>
2018-09-24 01:01:55 -0600

    funfzehn

---
+++ quince.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+//argc obtner el número de argumentos totales pasados en la línea de comandos.
+//es un array con los argumentos pasados en la línea de comando.
+int main(int argc, char *argv[])
+{
+
+  system("pause");
+  return 0;
+}

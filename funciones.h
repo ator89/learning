1fed8d8f5a5a4c37ce6334a2d1d4969e838f56ec
Angel Torres <ator89@outlook.com>
2018-09-24 01:43:29 -0600

    exe

--- funciones.h
+++ funciones.h
@@ -6,3 +6,8 @@ using namespace std;
 void printl(string str){
    cout << str << endl;
 }
+
+int sumar(int n1, int n2)
+{
+  return n1 + n2;
+}
--- sechzehn.cpp
+++ sechzehn.cpp
@@ -3,7 +3,14 @@
 
 using namespace std;
 
-int main()
+int main(int argc, char *argv[])
 {
+  
+  printl("hola");
+  int total = sumar(3,5);
+  cout << total<<endl;
+  
+  cin.get();
+  //system("pause");
   return 0;
 }

8481d733a4faede9a2be53e52f3ff22b4a619770
Angel Torres <ator89@outlook.com>
2018-09-24 01:13:46 -0600

    exe

--- quince.cpp
+++ quince.cpp
@@ -11,7 +11,14 @@ int main(int argc, char *argv[])
   for(int i=0; i<argc; i++){
      cout<< i << "argumento: " << argv[i] << endl;
   }
+  
+  if(argc>=3){
+     int n1 = atoi(argv[1]);
+     int n2 = atoi(argv[2]);
+      cout << "La suma total de " << n1 << "+" << n2 << "=" << n1+n2<< endl;
+  }
 
+  
   system("pause");
   return 0;
 }

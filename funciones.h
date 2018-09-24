3af4c8b092ae912554e5ae18c1b24176acd57761
Angel Torres <ator89@outlook.com>
2018-09-24 01:16:59 -0600

    funciones

---
+++ funciones.h
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void printl(string str){
+   cout << str << endl;
+}
---
+++ sechzehn.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "funciones.h";
+
+using namespace std;
+
+int main()
+{
+  return 0;
+}

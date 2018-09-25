e4ca8058acde18b62670a30e512084ab8385ad7a
Angel Torres <ator89@outlook.com>
2018-09-24 22:44:56 -0600

    18

---
+++ achtzehn.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "person.h"
+
+using namespace std;
+
+int main(int argc, char *argv[]){
+
+   Persona p;
+   cout << "Una persona: " << p.ojos()<<endl;
+   cout << p.brazos()<<endl;
+   cout << p.cabeza() << endl;
+   
+   p.color_ojos = "lokokok";
+   cout << p.color_ojos << endl;
+   cout << p.color_pelo << endl;
+   cout << p.altura(43.5) << endl;
+   cout << p.peso(138.7) << endl;
+   p.edad = 88;
+   cout << p.edad << endl;
+   
+   
+   system("pause");
+   return 0;
+}
---
+++ person.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+class Persona{
+      private:
+              int _ojos;
+              int _brazos;
+              int _cabeza;
+              
+      public:
+             int ojos(){
+                 
+                 return _ojos;
+                 };
+                 
+             int brazos();
+             int cabeza() { return _cabeza; };
+             static string color_ojos;
+             static string color_pelo;
+             float altura(float a){return a;};
+             float peso(float p);
+             static int edad;
+      };
+      
+Persona::Persona()
+{
+   _ojos = 2;
+   _brazos = 2;
+   _cabeza = 1;
+}
+      
+int Persona::brazos()
+{
+   return _brazos;
+}
+
+float Persona::peso(float p)
+{
+   return p;
+}
+
+string Persona::color_ojos = "negro";
+string Persona::color_pelo = "white";
+int Persona::edad = 0;

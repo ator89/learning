adbea70a25e6855399a5228655543244f16f985d
Angel Torres <ator89@outlook.com>
2018-09-25 19:59:34 -0600

    rectangulo

---
+++ ejercicios/rectangulo.h
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+class Punto
+{
+public:
+       void configurarX(int x){_x=x;};
+       void configurarY(int Y){_y=y;}; 
+      
+  private:
+     int _x;
+     int _y;       
+};
+

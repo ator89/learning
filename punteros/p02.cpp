ea012dd92fd8a46c427ad598dfbf6573942893c5
Angel Torres <ator89@outlook.com>
2018-09-26 07:45:21 -0600

    cambiar valor mediante puntero

--- punteros/p02.cpp
+++ punteros/p02.cpp
@@ -13,7 +13,14 @@ int main()
   cout << "Edad: " << edad1 << endl;
   pEdad = &edad1; //asignar la dirección de edad1 a pEdad
   
-  cout << "Valor puntero " << pEdad << endl;
+  cout << "Direccion en memoria del puntero " << pEdad << endl;
+  cout << "*pEdad: " << *pEdad << endl;
+  
+  cout << "Set *pEdad = 7 " << endl;
+  *pEdad = 7;
+  
+  cout << "*pEdad: " << *pEdad <<endl;
+  cout << "edad: " << edad1 << endl;
   
   system("pause");
   return 0;

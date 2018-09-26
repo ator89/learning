09a0d55ef944a1b1393913a715d51796a373312b
Angel Torres <ator89@outlook.com>
2018-09-26 00:52:30 -0600

    punteros

---
+++ punteros/p01.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  unsigned short n1 = 4;
+  unsigned long n2 = 54465;
+  long varNeg = -65535;
+  
+  cout << "Número 1: \t" << n1<<endl;
+  cout << "Direccion de n1: \t";
+  cout << &n1<< endl;
+  
+  
+  
+  
+  
+  
+  
+  system("pause");
+  return 0;
+}
